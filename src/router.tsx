cbb3274904ac7e8e2d08903d43f2da19cd128f8d
Nobeen Islam <n.islam.1@outlook.com>
2025-08-08 21:20:38 +0100

    Move subpages into subpages folder

--- src/router.tsx
+++ src/router.tsx
@@ -3,15 +3,15 @@ import { RootLayout } from "@/layouts/RootLayout";
 import { Home } from "@/pages/Home";
 import { Checklist } from "@/pages/Checklist";
 import { Makkah } from "@/pages/Makkah";
-import { Medina } from "@/pages/Medina";
+import { Medina } from "@/pages/Medina/Medina";
 import { About } from "@/pages/About";
 import { SecondaryLayout } from "@/layouts/SecondaryLayout";
-import { MasjidAnNabawi } from "@/pages/Medina/MasjidAnNabawi";
-import { GivingSalawaat } from "@/pages/Medina/GivingSalawaat";
-import { Rawdah } from "@/pages/Medina/Rawdah";
-import { AlBaqee } from "@/pages/Medina/AlBaqee";
-import { RecommendedActsMedina } from "@/pages/Medina/RecommendedActsMedina";
-import { MedinaZiyarah } from "@/pages/Medina/MedinaZiyarah";
+import { MasjidAnNabawi } from "@/pages/Medina/SubPages/MasjidAnNabawi";
+import { GivingSalawaat } from "@/pages/Medina/SubPages/GivingSalawaat";
+import { Rawdah } from "@/pages/Medina/SubPages/Rawdah";
+import { AlBaqee } from "@/pages/Medina/SubPages/AlBaqee";
+import { RecommendedActsMedina } from "@/pages/Medina/SubPages/RecommendedActsMedina";
+import { MedinaZiyarah } from "@/pages/Medina/SubPages/MedinaZiyarah";
 
 export const router = createBrowserRouter([
   {
